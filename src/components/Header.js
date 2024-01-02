c9fb32f2e60bb8f17562167acb0b7268b9c3bc08
Danial Habib <danialads12@gmail.com>
2024-01-02 21:18:27 +0500

    react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+// import components
+import Logo from "../assets/img/logo.svg";
+import Nav from "./Nav";
+
+// import icons
+import { HiMenu } from "react-icons/hi";
+
+const Header = ({ setNavMobile }) => {
+  return (
+    <header className="py-6" data-aos='fade-down' data-aos-duration='2000' data-aos-delay='900' >
+      <div className=" container mx-auto " >
+        <div className="flex items-center justify-between" >
+          {/* Logo */}
+          <a href="#" >
+            <img src={Logo} alt="logo" className="h-[30px]" />
+          </a>
+          {/* Nav */}
+          <Nav />
+          {/* Nav mobile btn */}
+          <HiMenu onClick={() => setNavMobile(true)} className=" lg:hidden text-3xl text-white cursor-pointer "/>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
